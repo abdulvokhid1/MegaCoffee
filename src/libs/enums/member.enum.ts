9185efbf6b9e7608a31f702c475aba4e950fd09e
ali <ibrakhimov7747a@gmail.com>
2024-06-26 05:50:07 +0900

    feat: added some features

--- src/libs/enums/member.enum.ts
+++ src/libs/enums/member.enum.ts
@@ -1,10 +1,10 @@
 export enum MemberType {
-    USER = "USER",
-    RESTAURANT= "RESTAURANT",
+  USER = "USER",
+  COFFEESHOP = "COFFEESHOP",
 }
 
 export enum MemberStatus {
-    ACTIVE = "ACTIVE",
-    BLOCK = "BLOCK",
-    DELETE = "DELETE"
-} 
+  ACTIVE = "ACTIVE",
+  BLOCK = "BLOCK",
+  DELETE = "DELETE",
+}
